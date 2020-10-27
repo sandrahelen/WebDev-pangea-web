43577ccdd8d4855456e354eb811607805934df90
Marit <maritfha@stud.ntnu.no>
2020-10-27 11:01:15 +0100

    #18 endet uri i apollo client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, createHttpLink } from '@apollo/client';
+
+const customFetch = (uri:string, options:any) => {
+  return fetch(uri, options)
+  .then(response => {
+    if (response.status >= 500) {  // or handle 400 errors
+      return Promise.reject(response.status);
+    }
+    return response;
+  });
+};
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000',
+  link: createHttpLink({
+    uri: "http://localhost:4000/graphql",
+    fetch: customFetch,
+  }),
   cache: new InMemoryCache()
 });
 
