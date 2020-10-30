065c75cae2f31f60ff0e93bc55b07f7cf6e39131
Sandra <sandra@Sandras-MacBook-Pro.local>
2020-10-30 11:10:00 +0100

    #6 tester for filtrering, søk, og brukere

--- frontend/cypress/integration/spec.ts
+++ frontend/cypress/integration/spec.ts
@@ -14,8 +14,14 @@ describe('End-2-End test', () => {
         cy.url().should('include', 'alle');
     });
 
-    /*
-    it('should open in browser', () => {
+    it('should filter countries by continent', () => {
+        cy.contains("Alle land").click();
+        cy.contains("Oceania").click();
+        cy.contains("New Zealand").should('exist');
+        cy.contains("Norway").should('not.exist');
+    });
+
+    it("should get error, user doesn't exist", () => {
         cy.contains("Logg inn").click();
         cy.url().should('include', 'login');
         cy.get('input[type=text]').type(username);
@@ -24,7 +30,6 @@ describe('End-2-End test', () => {
             .should('contain', 'User does not exist')
             .and('be.visible');
     });
-     */
 
     it('should register new user', () => {
         cy.contains("Registrer").click();
@@ -39,12 +44,39 @@ describe('End-2-End test', () => {
         cy.url().should('include', 'login');
         cy.get('input[type=text]').type(username);
         cy.get('input[type=submit]').click();
+        cy.contains("Logg ut").click();
+    });
+
+    it('should get error, user exists', () => {
+        cy.contains("Regitrer").click();
+        cy.get('input[type=text]').type(username);
+        cy.get('input[type=submit]').click();
+        cy.get('notification-message').children()
+            .should('contain', 'User already exists')
+            .and('be.visible');
     });
 
     it('should see visited countries', () => {
+        cy.contains("Logg inn").click();
+        cy.get('input[type=text]').type(username);
+        cy.get('input[type=submit]').click();
         cy.contains("Besøkte land").click();
         cy.url().should('include', 'mine');
         cy.contains("Logg ut").click();
         cy.url().should('include', 'alle');
+        cy.contains('Logg ut').click();
+    });
+
+    it('should add visited country', function () {
+        cy.contains("Logg inn").click();
+        cy.get('input[type=text]').type(username);
+        cy.get('input[type=submit]').click();
+        cy.contains("Besøkte land").click();
+        //TO-DO
+        cy.contains('Logg ut').click();
+    });
+
+    it('should search for country', function () {
+        //TO-DO
     });
 });
